3783d18125c081e8ad41d76175c628c5e50422e6
shockapools <shockapoolshawaii@gmail.com>
2026-04-27 23:50:08 -1000

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://shockapools.com',
-  integrations: [sitemap()],
-  trailingSlash: 'ignore'
-});
